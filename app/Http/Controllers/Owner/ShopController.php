e521e4b2bd3eacb404c30618dbd509a67c479c1e
naonao-aaa <naonao1814naonao@gmail.com>
2023-06-03 16:57:24 +0900

    Shop コントローラミドルウェア

--- app/Http/Controllers/Owner/ShopController.php
+++ app/Http/Controllers/Owner/ShopController.php
@@ -13,6 +13,22 @@ class ShopController extends Controller
     {
         $this->middleware('auth:owners');
 
+        $this->middleware(function ($request, $next) {
+            // dd($request->route()->parameter('shop')); //文字列
+            // dd(Auth::id()); //数字
+
+            $id = $request->route()->parameter('shop'); //shopのid取得
+            if(!is_null($id)){ // null判定
+                $shopsOwnerId = Shop::findOrFail($id)->owner->id;
+                $shopId = (int)$shopsOwnerId; // キャスト 文字列→数値に型変換
+                $ownerId = Auth::id();
+                if($shopId !== $ownerId){ // 同じでなかったら
+                    abort(404); // 404画面表示
+                }
+            }
+            return $next($request);
+        });
+
     } 
 
     public function index()
@@ -26,7 +42,7 @@ class ShopController extends Controller
 
     public function edit($id)
     {
-        //dd(Shop::findOrFail($id));
+        dd(Shop::findOrFail($id));
     }
 
     public function update(Request $request, $id)

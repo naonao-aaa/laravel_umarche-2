5d54b0b3cf895557cabe44d33afcaf0853842e38
naonao-aaa <naonao1814naonao@gmail.com>
2023-06-10 11:53:56 +0900

    Stripe 決済成功時の処理

--- app/Http/Controllers/User/CartController.php
+++ app/Http/Controllers/User/CartController.php
@@ -102,7 +102,7 @@ class CartController extends Controller
             'payment_method_types' => ['card'],
             'line_items' => [$lineItems],
             'mode' => 'payment',
-            'success_url' => route('user.items.index'),
+            'success_url' => route('user.cart.success'),
             'cancel_url' => route('user.cart.index'),
         ]);
 
@@ -112,5 +112,12 @@ class CartController extends Controller
             compact('session', 'publicKey'));
 
 
+    }
+
+    public function success()
+    {
+        Cart::where('user_id', Auth::id())->delete();
+
+        return redirect()->route('user.items.index');
     }
 }
